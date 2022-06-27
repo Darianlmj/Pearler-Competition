f93de83bc2d9b9bd28758a1d4c02de567fa6fa70
Darian Lee <darianlmj@gmail.com>
2022-06-27 18:31:34 +1000

    feat(frontend): made a transition for each tutorial question

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,62 +10,124 @@ const Home = () => {
   const [targetAge, setTargetAge] = useState(0);
   const [savingsGoal, setSavingsGoal] = useState(0);
 
+  const [isTutorialStarted, setIsTutorialStarted] = useState(false);
+  const [showWelcomeMessage, setShowWelcomeMessage] = useState(true);
+  const [isStage1Done, setIsStage1Done] = useState(false);
+  const [isStage2Done, setIsStage2Done] = useState(false);
+  const [isStage3Done, setIsStage3Done] = useState(false);
+  const [isTutorialFinished, setIsTutorialFinished] = useState(false);
+
+  const startTutorial = () => {
+    setIsTutorialStarted(true);
+    setShowWelcomeMessage(false);
+  }
+
   const calcSavingsPerWeek = () => {
     return (savingsGoal / ((targetAge - currentAge) * 52)).toFixed(2);
+  }
+
+  const checkStage1 = () => {
+    if (currentAge > 0) {
+      setIsTutorialStarted(false);
+      setIsStage1Done(true);
+    }
+  }
+
+  const checkStage2 = () => {
+    if (currentAge > 0 && savingsGoal > 0 && isStage1Done) {
+      setIsStage1Done(false);
+      setIsStage2Done(true);
+    }
+  }
+
+  const checkStage3 = () => {
+    if (currentAge > 0 && targetAge > 0 && savingsGoal > 0) {
+      setIsStage2Done(false);
+      setIsStage3Done(true);
+      setIsTutorialFinished(true);
+    }
   }
 
   return (
     <div className={styles.flexBox}>
       {/* Welcome Message */}
-      <div>
-        <h1 className={styles.header}>Hi There!</h1>
-        <h3 className={styles.subheader}>Let's get started shall we?</h3>
-        <button className={styles.button}>
-          <p className={styles.buttonText}>Begin</p>
-          <FontAwesomeIcon icon={faArrowRight as IconProp} />
-        </button>
-      </div>
+      {showWelcomeMessage &&
+        <div>
+          <h1 className={styles.header}>Hi There!</h1>
+          <p className={styles.subheader}>Let's get started shall we?</p>
+          <button className={styles.button} onClick={startTutorial}>
+            <p className={styles.buttonText}>Begin</p>
+            <FontAwesomeIcon icon={faArrowRight as IconProp} />
+          </button>
+        </div>
+      }
 
       {/* Question about Age */}
-      <div>
-        <h1 className={styles.header}>How old are you?</h1>
-        <h3 className={styles.subheader}>Please enter your age below.</h3>
-        <input className={styles.input} type='number' placeholder='Age' onChange={e =>{setCurrentAge(e.target.valueAsNumber)}} />
-        <button className={styles.button}>
-          <p className={styles.buttonText}>Continue</p>
-          <FontAwesomeIcon icon={faArrowRight as IconProp} />
-        </button>
-      </div>
+      {isTutorialStarted && 
+        <div>
+          <h1 className={styles.header}>How old are you?</h1>
+          <p className={styles.subheader}>Please enter your age below.</p>
+          <input className={styles.input} type='number' placeholder='Age' onChange={e =>{setCurrentAge(e.target.valueAsNumber)}} />
+          <button className={styles.button} onClick={checkStage1}>
+            <p className={styles.buttonText}>Continue</p>
+            <FontAwesomeIcon icon={faArrowRight as IconProp} />
+          </button>
+        </div>
+      }
+
 
       {/* Question about saving goals */}
-      <div>
-        <h1 className={styles.header}>How much do you want to save?</h1>
-        <h3 className={styles.subheader}>Please enter your saving goal below.</h3>
-        <input className={styles.input} type='number' placeholder='Goal' onChange={e =>{setSavingsGoal(e.target.valueAsNumber)}} />
-        <h3 className={styles.subheader}>By the time I'm</h3>
-        <input className={styles.input} type='number' placeholder='Target Age' onChange={e =>{setTargetAge(e.target.valueAsNumber)}} />
-        <button className={styles.button}>
-          <p className={styles.buttonText}>Continue</p>
-          <FontAwesomeIcon icon={faArrowRight as IconProp} />
-        </button>
-      </div>
+      {isStage1Done &&
+        <div>
+          <h1 className={styles.header}>How much do you want to save?</h1>
+          <p className={styles.subheader}>Please enter your saving goal below.</p>
+          <input className={styles.input} type='number' placeholder='Goal' onChange={e => {setSavingsGoal(e.target.valueAsNumber)}} />
+          <button className={styles.button} onClick={checkStage2}>
+            <p className={styles.buttonText}>Continue</p>
+            <FontAwesomeIcon icon={faArrowRight as IconProp} />
+          </button>
+        </div>
+      }
 
-      {/* Displaying the initial info */}
-      <div>
-        <h1 className={styles.header}>Preliminary Information</h1>
-        <h3 className={styles.subheader}>Current Age: {currentAge} years old</h3>
-        <h3 className={styles.subheader}>Target Age: ${savingsGoal}</h3>
-        <h3 className={styles.subheader}>Savings Goal: {targetAge} years old</h3>
-      </div>
 
-      {/* Dashboard for stats */}
-      <div>
-        <p className={styles.subheader2}>Based on the information provided...</p>
-        <h1 className={styles.header}>Pure Savings</h1>
-        <p className={styles.subheader}>
-          If you save just ${calcSavingsPerWeek()} a week, you can easily achieve your goals in {targetAge - currentAge} years.
-        </p>
-      </div>
+      {isStage2Done &&
+        <div>
+          <h1 className={styles.header}>Target Age</h1>
+          <p className={styles.subheader}>When do you aim to achieve this goal?</p>
+          <input className={styles.input} type='number' placeholder='Target Age' onChange={e => {setTargetAge(e.target.valueAsNumber)}} />
+          <button className={styles.button} onClick={checkStage3}>
+            <p className={styles.buttonText}>Continue</p>
+            <FontAwesomeIcon icon={faArrowRight as IconProp} />
+          </button>
+        </div>
+      }
+
+      {isTutorialFinished && 
+        <>
+          {/* Displaying the initial info */}
+          <div>
+            <h1 className={styles.header}>Preliminary Information</h1>
+            <p className={styles.subheader}>Current Age: {currentAge} years old</p>
+            <p className={styles.subheader}>Target Age: {targetAge} years old</p>
+            <p className={styles.subheader}>Savings Goal: ${savingsGoal}</p>
+          </div>
+        
+
+          {/* Dashboard for stats */}
+          <div>
+            <p className={styles.subheader2}>Based on the information provided...</p>
+            <h1 className={styles.header}>Pure Savings</h1>
+            <p className={styles.subheader}>
+              If you save just 
+              <span className={styles.highlight}> ${calcSavingsPerWeek()} </span>
+              a week, you can easily achieve your goals in
+              <span className={styles.highlight}> {targetAge - currentAge} </span>
+              years.
+            </p>
+            
+            <h1 className={styles.header}>Investing with Pearler Micro</h1>
+          </div>
+        </>}
     </div>
   )
 }
